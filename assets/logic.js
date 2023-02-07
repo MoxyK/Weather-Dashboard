68a93ae1caa026dab8f3be37c27770e34552289b
Moxy <keanu.knighton@gmail.com>
2023-02-07 16:09:08 +0000

    Added moment.js elements to show current date as well as dates for forecasted days

--- assets/logic.js
+++ assets/logic.js
@@ -1,5 +1,13 @@
 let apiKey = "5a5902899790712028b65ee43ec4eb64";
 
+let today = moment();
+console.log(today.format('dddd Do MMMM, YYYY'));
+let tomorrow = moment().add(1, 'days');
+let tomorrow2 = moment().add(2, 'days');
+let tomorrow3 = moment().add(3, 'days');
+let tomorrow4 = moment().add(4, 'days');
+let tomorrow5 = moment().add(5, 'days');
+
 $(document).ready(function() {
     $("#submit").click(function() {
         let location = $("#location").val();
@@ -22,7 +30,7 @@ $(document).ready(function() {
 })
 
 function outputData(response) {
-    return "<div><h2>Weather in " + response.name + " today" + "</h2>" + 
+    return "<div><h2>Weather in " + response.name + ": " + today.format('dddd Do MMMM, YYYY') + "</h2>" + 
     "<img src='http://openweathermap.org/img/w/" + response.weather[0].icon + ".png' width=100px" + "<br>" +
     "<h4> Weather: " + response.weather[0].main + "<br>" +
     "Description: " + response.weather[0].description + "<br>" +
@@ -60,7 +68,7 @@ $(function(){
     });
 
     function forecastData(response) {
-        return "<div><h3>Weather forecast for " + response.list[6].dt_txt + "</h3>" + 
+        return "<div><h3>Weather forecast for: " + tomorrow.format('dddd Do MMMM, YYYY') + "</h3>" + 
         "<img src='http://openweathermap.org/img/w/" + response.list[6].weather[0].icon + ".png' width=75px" + "<br>" +
         "<h4> Weather: " + response.list[6].weather[0].main + "<br>" +
         "Description: " + response.list[6].weather[0].description + "<br>" +
@@ -68,7 +76,7 @@ $(function(){
         "Humidity: " + response.list[6].main.humidity + "% <br>" +
         "Wind Speed: " + response.list[6].wind.speed + " m/sec </h4></div>" + "<br>" +
 
-        "<div><h3>Weather forecast for " + response.list[14].dt_txt + "</h3>" + 
+        "<div><h3>Weather forecast for: " + tomorrow2.format('dddd Do MMMM, YYYY') + "</h3>" + 
         "<img src='http://openweathermap.org/img/w/" + response.list[14].weather[0].icon + ".png' width=75px" + "<br>" +
         "<h4> Weather: " + response.list[14].weather[0].main + "<br>" +
         "Description: " + response.list[14].weather[0].description + "<br>" +
@@ -76,7 +84,7 @@ $(function(){
         "Humidity: " + response.list[14].main.humidity + "% <br>" +
         "Wind Speed: " + response.list[14].wind.speed + " m/sec </h4></div>" + "<br>" + 
 
-        "<div><h3>Weather forecast for " + response.list[22].dt_txt + "</h3>" + 
+        "<div><h3>Weather forecast for: " + tomorrow3.format('dddd Do MMMM, YYYY') + "</h3>" + 
         "<img src='http://openweathermap.org/img/w/" + response.list[22].weather[0].icon + ".png' width=75px" + "<br>" +
         "<h4> Weather: " + response.list[22].weather[0].main + "<br>" +
         "Description: " + response.list[22].weather[0].description + "<br>" +
@@ -84,7 +92,7 @@ $(function(){
         "Humidity: " + response.list[22].main.humidity + "% <br>" +
         "Wind Speed: " + response.list[22].wind.speed + " m/sec </h4></div>" + "<br>" + 
 
-        "<div><h3>Weather forecast for " + response.list[30].dt_txt + "</h3>" + 
+        "<div><h3>Weather forecast for: " + tomorrow4.format('dddd Do MMMM, YYYY') + "</h3>" + 
         "<img src='http://openweathermap.org/img/w/" + response.list[30].weather[0].icon + ".png' width=75px" + "<br>" +
         "<h4> Weather: " + response.list[30].weather[0].main + "<br>" +
         "Description: " + response.list[30].weather[0].description + "<br>" +
@@ -92,7 +100,7 @@ $(function(){
         "Humidity: " + response.list[30].main.humidity + "% <br>" +
         "Wind Speed: " + response.list[30].wind.speed + " m/sec </h4></div>" + "<br>" + 
 
-        "<div><h3>Weather forecast for " + response.list[38].dt_txt + "</h3>" + 
+        "<div><h3>Weather forecast for: " + tomorrow5.format('dddd Do MMMM, YYYY') + "</h3>" + 
         "<img src='http://openweathermap.org/img/w/" + response.list[38].weather[0].icon + ".png' width=75px" + "<br>" +
         "<h4> Weather: " + response.list[38].weather[0].main + "<br>" +
         "Description: " + response.list[38].weather[0].description + "<br>" +
